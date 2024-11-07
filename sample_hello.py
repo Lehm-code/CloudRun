20dab4bb673c7ddb0069e7ea621cd4997b37e3a2
Lehm-win <kira.free.masato@gmail.com>
2024-11-07 23:17:06 +0900

    apiテスト

--- sample_hello.py
+++ sample_hello.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World! from FastAPI"}
+def read_root(last_name: Optional[str] = None, first_name: Optional[str] = None):
+    if last_name and first_name:
+        return {"message": f"Hello, {last_name} {first_name}! from FastAPI"}
+    else:
+        return {"message": "Hello, World! from FastAPI"}
