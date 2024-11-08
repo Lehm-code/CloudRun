ced61117c98c21fe73050d09b45d6116a036886c
Lehm-win <kira.free.masato@gmail.com>
2024-11-08 09:37:04 +0900

    postに対応

--- sample_hello.py
+++ sample_hello.py
@@ -1,20 +1,12 @@
-from fastapi import FastAPI
-from typing import Optional
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-# # CORSの設定
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # 必要に応じて許可するオリジンを指定
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-@app.get("/")
-def read_root(last_name: Optional[str] = None, first_name: Optional[str] = None):
+@app.post("/")
+async def read_root(request: Request):
+    data = await request.json()
+    last_name = data.get("lastName")
+    first_name = data.get("firstName")
     if last_name and first_name:
         return {"message": f"Hello, {last_name} {first_name}! from FastAPI"}
     else:
