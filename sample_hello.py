faea9312cb0e3d53383993cf09fd1808084e8baa
Lehm-win <kira.free.masato@gmail.com>
2024-11-09 14:18:31 +0900

    エラーハンドリング

--- sample_hello.py
+++ sample_hello.py
@@ -1,17 +1,20 @@
 import logging
+import google.cloud.logging
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-# ロガーの設定
-logging.basicConfig(level=logging.INFO)  # INFOレベル以上のログを記録
+# Cloud Loggingハンドラを標準loggingモジュールに接続する
+log_client = google.cloud.logging.Client()
+log_client.setup_logging()
+
+# ロガーを取得
 logger = logging.getLogger(__name__)
 
 @app.post("/")
 async def read_root(request: Request):
     # リクエストの受信をログに記録
-    logger.info("Received a request")
-    print('ログ出力テスト')
+    logger.info("リクエストを受信しました")
     try:
         # JSONデータの取得と解析
         data = await request.json()
@@ -19,19 +22,18 @@ async def read_root(request: Request):
         first_name = data.get("firstName")
         
         # デバッグ情報のログ記録
-        logger.debug(f"Request Data: {data}")
+        logger.debug(f"リクエストデータ: {data}")
         
         # 名前が存在する場合の処理
         if last_name and first_name:
-            message = f"Hello, {last_name} {first_name}! from FastAPI"
-            logger.info(f"Responding with message: {message}")
+            message = f"Hello, {last_name} {first_name}! from Python"
+            logger.info(f"返信メッセージ: {message}")
             return {"message": message}
         else:
             # 名前がない場合の処理
-            logger.warning("No name provided in request")
-            return {"message": "Hello, World! from FastAPI"}
-
+            logger.warning("名前が入力されていません。")
+            return {"message": "Hello, World! from Python"}
     except Exception as e:
         # 例外発生時のエラーログ記録
-        logger.error("An error occurred", exc_info=True)
+        logger.error("エラー発生", exc_info=True)
         return {"message": "An error occurred during processing"}
